cb3ba28c0647cbd60f97df7e611689de3325639d
Utsav <utsavbajra619@gmail.com>
2025-02-26 12:35:35 -0500

    add mobile menu hamburger icon

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
     {!isLoaded && <LoadingScreen onComplete= {() => setIsLoaded(true)} />}{" "}
     <div 
     className={'min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100'}>
 
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,11 @@
-export const Navbar = () => {
+import { useEffect } from "react"
+
+export const Navbar = ({menuOpen, setMenuOpen}) => {
+
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
+
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-white/10 shadow-lg">
 
@@ -9,7 +16,7 @@ export const Navbar = () => {
                          Utsav<span className="text-blue-500">.Bajra</span>{" "}
                     </a>
                     
-                    <div className="w-7 h-5  cursor-pointer z-40 md:hidden">
+                    <div className="w-7 h-5  cursor-pointer z-40 md:hidden" onClick={() =>  setMenuOpen((prev) => !prev)} >
                         &#9776;
                     </div>
 
