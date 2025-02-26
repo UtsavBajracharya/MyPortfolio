54ece459096fa8202e617984e53b9c9f55454a7c
Utsav <utsavbajra619@gmail.com>
2025-02-26 13:12:36 -0500

    change menu open close icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import { LoadingScreen } from './components/LoadingScreen';
 import { Navbar} from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 import "./index.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
     className={'min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100'}>
 
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
     </>
   );
